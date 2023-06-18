b9b58e5554ba40b0545c7593cdb1e5c36b08355c
Musab Hassan <musabhassan1999@gmail.com>
2023-06-18 13:00:34 +0530

    redux & JSON server

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,20 @@
+import { ReduxProvider } from "@/redux/provider";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-import { ReduxProvider } from '@/redux/provider'
-import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'MegaMart',
-  description: 'Devloped By Musab Hassan',
-}
+  title: "MegaMart",
+  description: "Devloped By Musab Hassan",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full bg-gray-100">
-      <body class="h-full" className={inter.className}>
+      <body className={inter.className}>
         <ReduxProvider>{children}</ReduxProvider>
-        </body>
+      </body>
     </html>
-  )
+  );
 }
--- src/redux/features/product-list/productListAPI.js
+++ src/redux/features/product-list/productListAPI.js
@@ -1,8 +1,22 @@
-export function fetchCount(amount = 1) {
-    return new Promise(async (resolve) =>{
-      const response = await fetch('http://localhost:8080') 
-      const data = await response.json()
-      resolve({data})
-    }
-    );
+export function fetchAllProducts() {
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8080/products");
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+
+export function fetchProductsByFilters(filter) {
+  // filter  = {'category' : 'samartphone'}
+  let queryString = '';
+  for(let key in filter){
+    queryString += `${key}=${filter[key]}&`;
   }
+
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8080/products?"+queryString);
+    const data = await response.json();
+    resolve({ data });
+  });
+}
